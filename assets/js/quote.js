f5d879075f7ee14ad7e9e88222ec08245672da23
ssesayjr <43327507+ssesayjr@users.noreply.github.com>
2023-06-13 05:18:52 -0400

    update about slider

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,118 +1,46 @@
-(function() {
-	var cdSlider = document.querySelector('.cd-slider'),
-		item = cdSlider.querySelectorAll("li"),
-		nav = cdSlider.querySelector("nav");
+var theSlide = 2,
+    numSlides = 5,
+    frequency = 6500,
+    lastButton = "#quote-1";
 
-	item[0].className = "current_slide";
+var slide = function(){
+    var	currentSlide = "#quote-"+theSlide+"-content",
+				lastSlide = lastButton+"-content",
+        currentButton = "#quote-"+theSlide;
 
-	for (var i = 0, len = item.length; i < len; i++) {
-		var color = item[i].getAttribute("data-color");
-		item[i].style.backgroundColor=color;
-	}
+    $(".slider-quote").removeClass("in-focus");
+    $(".quote-control").removeClass("selected");
+    $(currentSlide).addClass("in-focus");
+    $(currentButton).addClass("selected");
 
-	// Detect IE
-	// hide ripple effect on IE9
-	var ua = window.navigator.userAgent;
-		var msie = ua.indexOf("MSIE");
-		if ( msie > 0 ) {
-			var version = parseInt(ua.substring(msie+ 5, ua.indexOf(".", msie)));
-			if (version === 9) { cdSlider.className = "cd-slider ie9";}
-	}
 
-	if (item.length <= 1) {
-		nav.style.display = "none";
-	}
+    lastButton = currentButton;
+    if(theSlide < numSlides){
+        theSlide += 1;
+    }
+    else{
+        theSlide = 1;
+    }
+};
 
-	function prevSlide() {
-		var currentSlide = cdSlider.querySelector("li.current_slide"),
-			prevElement = currentSlide.previousElementSibling,
-			prevSlide = ( prevElement !== null) ? prevElement : item[item.length-1],
-			prevColor = prevSlide.getAttribute("data-color"),
-			el = document.createElement('span');
+var interval = setInterval(slide, frequency);
 
-		currentSlide.className = "";
-		prevSlide.className = "current_slide";
+$(function(){
 
-		nav.children[0].appendChild(el);
+    $("div#quote-slider").mouseover(function(){
+        clearInterval(interval);
+    }).mouseout(function(){
+        interval = setInterval(slide, frequency);
+    });
+  
+  	$(".quote-control").click(function(e){
+      	clearInterval(interval);
+        $(".slider-quote").removeClass("in-focus");
+    		$(".quote-control").removeClass("selected");
+      	var quoteSlider = "#"+$(this).attr("id")+"-content";
+      	$(quoteSlider).addClass("in-focus");
+      	$(this).addClass("selected");
+      	$("div#quote-slider").unbind('mouseover mouseout');
+    });
 
-		var size = ( cdSlider.clientWidth >= cdSlider.clientHeight ) ? cdSlider.clientWidth*2 : cdSlider.clientHeight*2,
-		    ripple = nav.children[0].querySelector("span");
-
-		ripple.style.height = size + 'px';
-		ripple.style.width = size + 'px';
-		ripple.style.backgroundColor = prevColor;
-
-		ripple.addEventListener("webkitTransitionEnd", function() {
-			if (this.parentNode) {
-				this.parentNode.removeChild(this);
-			}
-		});
-
-		ripple.addEventListener("transitionend", function() {
-			if (this.parentNode) {
-				this.parentNode.removeChild(this);
-			}
-		});
-
-	}
-
-	function nextSlide() {
-		var currentSlide = cdSlider.querySelector("li.current_slide"),
-			nextElement = currentSlide.nextElementSibling,
-			nextSlide = ( nextElement !== null ) ? nextElement : item[0],
-			nextColor = nextSlide.getAttribute("data-color"),
-			el = document.createElement('span');
-
-		currentSlide.className = "";
-		nextSlide.className = "current_slide";
-
-		nav.children[1].appendChild(el);
-
-		var size = ( cdSlider.clientWidth >= cdSlider.clientHeight ) ? cdSlider.clientWidth*2 : cdSlider.clientHeight*2,
-			  ripple = nav.children[1].querySelector("span");
-
-		ripple.style.height = size + 'px';
-		ripple.style.width = size + 'px';
-		ripple.style.backgroundColor = nextColor;
-
-		ripple.addEventListener("webkitTransitionEnd", function() {
-			if (this.parentNode) {
-				this.parentNode.removeChild(this);
-			}
-		});
-
-		ripple.addEventListener("transitionend", function() {
-			if (this.parentNode) {
-				this.parentNode.removeChild(this);
-			}
-		});
-
-	}
-
-	updateNavColor();
-
-	function updateNavColor () {
-		var currentSlide = cdSlider.querySelector("li.current_slide");
-
-		var nextColor = ( currentSlide.nextElementSibling !== null ) ? currentSlide.nextElementSibling.getAttribute("data-color") : item[0].getAttribute("data-color");
-		var	prevColor = ( currentSlide.previousElementSibling !== null ) ? currentSlide.previousElementSibling.getAttribute("data-color") : item[item.length-1].getAttribute("data-color");
-
-		if (item.length > 2) {
-			nav.querySelector(".prev").style.backgroundColor = prevColor;
-			nav.querySelector(".next").style.backgroundColor = nextColor;
-		}
-	}
-
-	nav.querySelector(".next").addEventListener('click', function(event) {
-		event.preventDefault();
-		nextSlide();
-		updateNavColor();
-	});
-
-	nav.querySelector(".prev").addEventListener("click", function(event) {
-		event.preventDefault();
-		prevSlide();
-		updateNavColor();
-	});
-
-})();
+});
