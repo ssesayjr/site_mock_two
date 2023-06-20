9b580d003083fd9942f7234841b5bbfc754c2862
ssesayjr <43327507+ssesayjr@users.noreply.github.com>
2023-06-20 16:16:43 -0400

    updated member access functionality

--- test/main.py
+++ test/main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import MySQLdb.cursors, re, hashlib
+# import sshtunnel
 
 # # host = "deitralou24907.domaincommysql.com", 
 # # user = "dreiser", 
@@ -17,6 +18,22 @@ import MySQLdb.cursors, re, hashlib
 # rows = c.fetchall()
 # for row in rows:
 #     print(row)
+
+# sshtunnel.SSH_TIMEOUT = 5.0
+# sshtunnel.TUNNEL_TIMEOUT = 5.0
+
+
+# with sshtunnel.SSHTunnelForwarder(
+#     ('www.domain.com'),
+#     ssh_username='deitralou24907', ssh_password='Transform4Equity$250',
+#     remote_bind_address=('deitralou24907.domaincommysql.com', 3306)
+# ) as tunnel:
+#     conn = MySQLdb.connect(
+#         user='dreiser',
+#         passwd='T4Equity2023!',
+#         host='deitralou24907.domaincommysql.com', port=tunnel.local_bind_port,
+#         db='member_portal',
+#     )
 
 
 app = Flask(__name__)
@@ -35,6 +52,7 @@ mysql = MySQL(app)
 print ("Content-type:text/html\n\n" )
 import MySQLdb 
 
+# WORKING CONFIGURATION
 conn = MySQLdb.connect (
   host = "localhost", 
   user = "root", 
@@ -42,6 +60,13 @@ conn = MySQLdb.connect (
   db = "transform") 
 
 
+## TESTING ALL THE CONFIGURATIONS
+# conn = MySQLdb.connect (
+#  #server = "custsql-dom03.eigbox.net",
+#  host = "deitralou24907.domaincommysql.com", 
+#  user = "ssesay", 
+#  passwd = "Leumas88!", 
+#  db = "member_portal")
 
 print ("connected to the database")
 c = conn.cursor()
@@ -84,6 +109,7 @@ def login():
             session['loggedin'] = True
             session['id'] = account['idusers']
             session['username'] = account['email']
+            session['status'] = account['active']
             # Redirect to home page
             return redirect(url_for('home'))
         else:
@@ -99,6 +125,7 @@ def logout():
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
+   session.pop('status', None)
    # Redirect to login page
    return redirect(url_for('login'))
 
@@ -117,10 +144,11 @@ def home():
 def resources():
     # Check if the user is logged in
     if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('resources.html', username=session['username'])
+            if 'status' in session:
+                # User is loggedin show them the home page
+                return render_template('resources.html', username=session['username'])
     # User is not loggedin redirect to login page
-    return redirect(url_for('index.html'))
+    return redirect(url_for('index'))
 
 
 # http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for logged in users
@@ -148,58 +176,48 @@ def sqltable():
         cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor2.execute('SELECT * FROM users')
         result = cursor2.fetchall()
-
-
        
- 
     return render_template('sqltable.html', sqltab=result)
 
 @app.route('/test/updateAccount', methods=['GET', 'POST'])
-def updateAccounnt():
+def updateAccount():
     if 'loggedin' in session:
         msg = ''
+
         # Check if "username", "password" and "email" POST requests exist (user submitted form)
-        if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        if request.method == 'POST' and 'email' in request.form and 'acctStatus' in request.form:
             # Create variables for easy access
-            username = request.form['username']
-            new_password = request.form['password']
-            email = request.form['email']
-            accID = request.form['accountID']
+            username = request.form['email']
             status = request.form['acctStatus']
 
             # Check if account exists using MySQL
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
             cursor.execute('SELECT * FROM users WHERE email = %s', (username,))
+            cursor2.execute('SELECT idusers FROM users WHERE email = %s', (username,))
+            id = cursor2.fetchone()['idusers']
             account = cursor.fetchone()
             # If account exists show error and validation checks
-            if account:
-                msg = 'Account already exists!'
-            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            if not username:
+                msg = 'Please fill out the email address!'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', username):
                 msg = 'Invalid email address!'
-            elif not re.match(r'[A-Za-z0-9]+', username):
-                msg = 'Username must contain only characters and numbers!'
-            elif not username or not new_password or not email:
+            elif not status:
+                msg = 'Please fill out the id!'
+            elif not username or not status:
                 msg = 'Please fill out the form!'
             else:
                 # Hash the password
 
                 # Account doesn't exist, and the form data is valid, so insert the new account into the accounts table
-                cursor.execute('UPDATE transform.users SET password = %s, status = %s WHERE email =%s and idusers=%s)', (new_password, status, email, accID))
+                cursor.execute('UPDATE users SET active = %s WHERE email =%s and idusers=%s', (status, username, id,))
                 mysql.connection.commit()
-                msg = 'You have successfully registered!'
+                msg = 'You have successfully updated the account'
 
 
         elif request.method == 'POST':
             # Form is empty... (no POST data)
             msg = 'Please fill out the form!'
         # Show registration form with message (if any)
-
-
-
-        
-
-
-
-
  
-    return render_template('sqltable.html', sqltab=result)
+    return render_template('profile.html', msg=msg)
